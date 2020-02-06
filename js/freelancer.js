3b1e3fe1252638525ad44aa7af773114ce1e6c29
dnrlwo <dnrlwo11@gmail.com>
2020-02-07 02:09:22 +0900

    js수정

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,12 +35,3 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
-
-const appearence = document.getElementById("appearence");
-function check() {
-    if (this.checked == true) {
-        appearence.setAttribute("style", "display:block;");
-    } else {
-        appearence.setAttribute("style", "display:none;");
-    }
-}
